1d8298339c4b0ebcfbfcda92631571e59b4d20c7
Jake Westerfield <jecobwesterfield@gmail.com>
2017-06-24 16:53:22 -0700

    update.
    update

--- Batch3.bat
+++ Batch3.bat
@@ -3,7 +3,7 @@ title My Test Batch File
 :: See the title at the top.
 echo Test file executed.
 echo I am too lazy to write commands again and again.
-
-START C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat
-Call VsDevCmd.bat
+::START C:\"Program Files (x86)"\Google\Chrome\Application\chrome.exe
+::START C:\Users\"TechHire User"\AppData\Local\slack\slack.exe
+START C:\"Program Files (x86)"\"Microsoft Visual Studio 14.0"\Common7\Tools\VsDevCmd.bat
 pause

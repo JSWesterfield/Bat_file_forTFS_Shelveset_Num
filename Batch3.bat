8b96c9778bbd7b21f80ca381c260d87fbb7a1b33
Jake Westerfield <jecobwesterfield@gmail.com>
2017-06-24 15:54:14 -0700

    Retrieving TFS Shelveset Num via batch file
    Retrieving TFS Shelveset #, and for sorting VS Solution via batch file.

---
+++ Batch3.bat
@@ -0,0 +1,9 @@
+echo off
+title My Test Batch File
+:: See the title at the top.
+echo Test file executed.
+echo I am too lazy to write commands again and again.
+
+START C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat
+Call VsDevCmd.bat
+pause
